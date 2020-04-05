683b44e41598c99747be054c1ba92dd66bfda8fb
Todd McGrath <mcgrath@supergloo.com>
2020-04-05 11:29:01 -0500

    Add voluble test data gen example Delete all topics script a bit more flexible

--- kafka-delete-all-topics.sh
+++ kafka-delete-all-topics.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+# Set you KAFKA_HOME to use
 
-TOPICS=$(./kafka-topics.sh --zookeeper 0.0.0.0:2181 --list )
+TOPICS=$($KAFKA_HOME/bin/kafka-topics.sh --zookeeper 0.0.0.0:2181 --list )
 
 for T in $TOPICS
 do
   if [ "$T" != "__consumer_offsets" ]; then
-    ./kafka-topics.sh --zookeeper 0.0.0.0:2181 --delete --topic $T
+    $KAFKA_HOME/bin/kafka-topics.sh --zookeeper 0.0.0.0:2181 --delete --topic $T
   fi
 done
